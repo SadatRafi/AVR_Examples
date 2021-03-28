26016af56673af73cb22cc31172e5bc624f0ade8
SadatRafi <sadatrafi1376@gmail.com>
2021-03-28 11:59:08 +0600

    Update Blink_By_Pointer.ino

--- Blink_By_Pointer/Blink_By_Pointer.ino
+++ Blink_By_Pointer/Blink_By_Pointer.ino
@@ -6,10 +6,12 @@
 void setup() 
 {
   (*(volatile uint8_t*)(0x24))|=(1<<5);
+  //Writing 1 to bit 5 of DDRB (For Arduino Pin 13)
 }
 
 void loop() 
 { 
   (*(volatile uint8_t*)(0x25))^=(1<<5);
+  //Writing 1 to bit 5 of PORTB (Arduino Pin 13)
   delay(500);
 }
