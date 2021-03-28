83d04fcc8a99489b1a92c1cc8793486141549159
SadatRafi <sadatrafi1376@gmail.com>
2021-03-28 11:56:41 +0600

    Update Blink_By_Pointer.ino

--- Blink_By_Pointer/Blink_By_Pointer.ino
+++ Blink_By_Pointer/Blink_By_Pointer.ino
@@ -1,3 +1,8 @@
+//This code explains how to use pointers to write desired values in a specific address of AVR Microcontroller
+//Boared used :Arduino Nano
+//MCU: ATMEGA328P
+//Compiler Arduino IDE
+
 void setup() 
 {
   (*(volatile uint8_t*)(0x24))|=(1<<5);
